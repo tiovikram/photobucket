b4ff1ae630411992c21c5a18e391f02ab2d1ab4e
Vikram Guhan Subbiah <vsubbiah@uw.edu>
2022-06-17 06:46:43 -0700

    master branch fix
    Extract files from within photobucket dir
    and remove .iml and .idea files (in .gitignore now)

---
+++ src/main/java/com/example/photobucket/PhotobucketApplication.java
@@ -0,0 +1,13 @@
+package com.example.photobucket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhotobucketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhotobucketApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/photobucket/PhotobucketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.photobucket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhotobucketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
